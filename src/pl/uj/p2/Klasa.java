1fef3109f99366990d8aba4655ebbc6e38a7e74a
indywidualny <graal13>
2015-05-07 00:12:06 +0200

    enums

--- src/pl/uj/p2/Klasa.java
+++ src/pl/uj/p2/Klasa.java
@@ -5,46 +5,30 @@ package pl.uj.p2;
 
 public enum Klasa {
 
-    Len(-1),
-    Mag(4),
-    Osilek(5),
-    Palladyn(2),
-    Szpieg(2);
+    Len(-1, ""),
+    Mag(4, "Ork", "Gnom"),
+    Osilek(5, "Gnom"),
+    Palladyn(2, "Elf"),
+    Szpieg(2, "Ork", "Krasnolud");
 
     private String[] nieakceptowalneRasy;
     private int	premiaDoPoziomu;
 
-    Klasa(int p) {
-        this.premiaDoPoziomu = p;
+    Klasa(int premiaDoPoziomu, String... nieakceptowalneRasy) {
+        this.premiaDoPoziomu = premiaDoPoziomu;
+        this.nieakceptowalneRasy = nieakceptowalneRasy;
     }
 
     public boolean czyRasaAkceptowalna(Rasa rasa) {
-        return false;
+        for (int i = 0; i < this.nieakceptowalneRasy.length; i++)
+            if (rasa != null && rasa.toString().equals(this.nieakceptowalneRasy[i]))
+                return false;
+
+        return true;
     }
 
     public int pobierzPremie() {
         return this.premiaDoPoziomu;
     }
-
-
-/*    // TODO: No way to override it so leave it default
-    public static Klasa valueOf(String name) {
-        if (name.equals("Len"))
-            return Len;
-        else if (name.equals("Mag"))
-            return Mag;
-        else if (name.equals("Osilek"))
-            return Osilek;
-        else if (name.equals("Palladyn"))
-            return Palladyn;
-        else if (name.equals("Szpieg"))
-            return Szpieg;
-        else return null;
-
-    }
-
-    public static Klasa[] values() {
-        return null;
-    }*/
 
 }
--- src/pl/uj/p2/Rasa.java
+++ src/pl/uj/p2/Rasa.java
@@ -5,21 +5,26 @@ package pl.uj.p2;
 
 public enum Rasa {
 
-    Czlowiek(-4),
-    Elf(3),
-    Gnom(5),
-    Krasnolud(2),
-    Ork(1);
+    Czlowiek(4, ""),
+    Elf(3, "Palladyn"),
+    Gnom(5, "Osilek"),
+    Krasnolud(2, "Szpieg"),
+    Ork(1, "Szpieg", "Mag");
 
-    private String[] nieakceptowalneRasy;
+    private String[] nieakceptowalneKlasy;
     private int	premiaDoPoziomu;
 
-    Rasa(int p) {
-        premiaDoPoziomu = p;
+    Rasa(int premiaDoPoziomu, String ... nieakceptowalneKlasy) {
+        this.premiaDoPoziomu = premiaDoPoziomu;
+        this.nieakceptowalneKlasy = nieakceptowalneKlasy;
     }
 
-    boolean	czyRasaAkceptowalna(Klasa klasa) {
-        return false;
+    public boolean czyKlasaAkceptowalna(Klasa klasa) {
+        for (int i = 0; i < this.nieakceptowalneKlasy.length; i++)
+            if (klasa != null && klasa.toString().equals(this.nieakceptowalneKlasy[i]))
+                return false;
+
+        return true;
     }
 
     int	pobierzPremie() {
